bc951444d480f0c6a6eb1a9608d33b15d0681dd1
Judy Tuan <judytuna@gmail.com>
2011-09-04 18:43:45 -0700

    did 5.5 exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   #return a title on a per-page basis
   def title
     base_title = "POVRay Shortcode Contest"
