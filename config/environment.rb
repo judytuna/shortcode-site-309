28549e9203473a760fb10f75b14accba2095d275
Judy Tuan <judytuna@gmail.com>
2011-09-05 01:12:46 -0700

    added paperclip to Gemfile and bundle installed, haven't used it yet though

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 PovrayShortcode::Application.initialize!
+
+Paperclip.options[:command_path] = "/opt/local/bin/"
