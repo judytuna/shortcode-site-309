4b9c5e5ae975b3ac2f33e54dffb49ade25e22ec6
Judy Tuan <judytuna@gmail.com>
2011-09-04 17:28:18 -0700

    done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 PovrayShortcode::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
+  get "pages/about"
+  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    #
+    # Define @base_title here.
+    #
+    @base_title = "POVRay Shortcode Contest"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Home")
     end
   end
 
@@ -14,6 +28,38 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Contact")
+    end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => @base_title + " | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Help")
+    end
+  end
 end
+
