2749b03dbee6dd350f8be26476c16afb7b8f41c2
Judy Tuan <judytuna@gmail.com>
2011-09-04 17:51:09 -0700

    went through chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #return a title on a per-page basis
+  def title
+    base_title = "POVRay Shortcode Contest"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
