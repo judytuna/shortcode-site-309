825324cf1b80fbb2d54a8c472526d16fcfd52a20
Judy Tuan <judytuna@gmail.com>
2011-09-04 18:14:49 -0700

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 PovrayShortcode::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
