becf2af01334936df4930c94cc904bb15c3e5e19
Peterson Trethewey <peterson.trethewey@gmail.com>
2011-09-15 19:46:26 -0700

    Added new entry button, and edit functionality for entries.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,13 +7,33 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry  = current_user.entries.build(params[:entry])
+    @entry = current_user.entries.build(params[:entry])
     if @entry.save
-      flash[:success] = "Entry created!"
+      flash[:success] = "Entry created."
       redirect_to current_user
     else
       render 'entries/new'
     end
   end
   
+  def update
+    @entry = Entry.find(params[:id])
+    if @entry.update_attributes(params[:entry])
+      flash[:success] = "Entry updated."
+      redirect_to current_user
+    else
+      render 'entries/edit'
+    end
+  end
+  
+  def edit
+    @entry = Entry.find(params[:id])
+    @title = "Edit Entry"
+  end
+  
+  def destroy
+    Entry.find(params[:id]).destroy
+    flash[:success] = "Entry deleted."
+    redirect_to current_user
+  end
 end
