8cd56dfc7d5060b604862c97623f406b72b0e155
Peterson Trethewey <peterson.trethewey@gmail.com>
2011-10-03 17:07:48 -0700

    Got vote button to AJAXify

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,12 +4,18 @@ class VotesController < ApplicationController
   def create
     @entry = Entry.find(params[:vote][:entry_id])
     current_user.vote!(@entry)
-    redirect_to @entry
+    respond_to do |format|
+      format.html {redirect_to @entry}
+      format.js
+    end
   end
   
   def destroy
     @entry = Vote.find(params[:id]).entry
     current_user.unvote!(@entry)
-    redirect_to @entry
+    respond_to do |format|
+      format.html {redirect_to @entry}
+      format.js
+    end
   end
 end
