31ddb3cb80050f164ba8cea0a7d4a03220c04b16
Peterson Trethewey <peterson.trethewey@gmail.com>
2011-12-05 12:19:47 -0800

    added function to application helper to stringify a time interval in a pleasing way.  Added to the header to show the time until the next deadline using that function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,5 +13,37 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  def time_between(timea, timeb)
+    difference = (timea - timeb).to_i
+    if difference < 0
+      difference *= -1
+    end
+    
+  	seconds    =  difference % 60
+	difference = (difference - seconds) / 60
+	minutes    =  difference % 60
+	difference = (difference - minutes) / 60
+	hours      =  difference % 24
+	difference = (difference - hours)   / 24
+	days       =  difference % 7
+	
+	result = ""
+	
+	if (days > 0)
+	  result << pluralize(days, 'day')
+	end
+	
+	if (days < 2)
+      result << ' ' << pluralize(days, 'hour')
+	end
+	
+	if (hours < 1)
+      result << ' ' << pluralize(days, 'minute') + ' ' + pluralize(days, 'second')
+	end
+	
+    return result
+  end
 
 end
+
